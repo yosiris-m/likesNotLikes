c230aacfb73ddf1f0ab58ff3f98857f77c54685e
Yosiris <yosiris1086@gmail.com>
2023-04-25 09:45:47 +0200

    test:  add test to components

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import ReactDOM from "react-dom";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders without crashing", () => {
+  const main = document.createElement("main");
+  render(<App />, main);
+  ReactDOM.unmountComponentAtNode(main);
 });
